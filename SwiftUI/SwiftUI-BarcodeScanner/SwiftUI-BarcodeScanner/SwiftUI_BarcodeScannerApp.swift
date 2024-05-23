5f073676e8d63792051bd3702dd760ee5f9e5cf6
Gerard <gersapinads@gmail.com>
2024-05-23 14:26:44 +0800

    [SwiftUI_Barcode] add project file

---
+++ SwiftUI/SwiftUI-BarcodeScanner/SwiftUI-BarcodeScanner/SwiftUI_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarcodeScannerApp.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Gerard on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
