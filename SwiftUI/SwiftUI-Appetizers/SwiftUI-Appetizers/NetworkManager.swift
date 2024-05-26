9fd6c6cb34c34bc75163d07fba6bf07af2b9388c
Gerard <gersapinads@gmail.com>
2024-05-26 15:39:53 +0800

    [SwiftUI_Appetizers] add download image api handler

--- SwiftUI/SwiftUI-Appetizers/SwiftUI-Appetizers/NetworkManager.swift
+++ SwiftUI/SwiftUI-Appetizers/SwiftUI-Appetizers/NetworkManager.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import UIKit
 
 final class NetworkManager {
     
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
@@ -48,6 +50,35 @@ final class NetworkManager {
             } catch {
                 completion(.failure(.invalidData))
             }
+        }
+        
+        task.resume()
+    }
+    
+    func downloadImage(urlString: String, completed: @escaping (UIImage?) -> Void ) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { [self] data, response, error in
+            guard let data = data,
+                  let image = UIImage(data: data)
+            else {
+                completed(nil)
+                return
+            }
+            
+            cache.setObject(image, forKey: cacheKey)
+            completed(image)
+            
         }
         
         task.resume()
